2cd026230e8431ad46d4b0d0c1131a107b702c41
Damien <teodam13@gmail.com>
2015-05-29 23:59:20 +0200

    up

--- fourmis/Matrix.py
+++ fourmis/Matrix.py
@@ -2,20 +2,33 @@ class Matrix(object):
 	# i = from
 	# j = to
     def __init__():
-    	self.table=[] 
+        self.table=[] 
+
     Matrix.add(1,2,1,3)
     add(1,2,PHENOM_PLUS,  2)
     add(1,2,PHENOM_MOINS, 2)
-    def set(i, j, k, ajout):
-    	if exists(i,j,k)
-    		#update
-    	else
-    		#add
-    	insert = [i,j,k]+[ajout];
-    	return self.table += insert
+
+    def exist(i,j)
+        
+
+
+    def setRetribution(previous, success):
+        n=len(self.table)
+        m=len(previous)
+        for i in range(0,n):
+            for j in range(0,m):
+                if (success==1):
+                   exist( self.table[i], (previous[i+1],previous[i]) )
+                else:
+
+
 
     def get(i,j,k):
-    	return
+        return
 
-    def erosion(j,coeff):
-    	
+    def erosion(j, coeff):
+        n = len(self.table)
+        for i in range(0,n):
+            if self.table[i][1]==j
+                self.table[i][2]*=0.9
+                self.table[i][3]*=0.9
--- fourmis/algos.py
+++ fourmis/algos.py
@@ -1,16 +1,3 @@
 class Algos(object):
-    def test(self):
-        return "MON TEST"
-
-
-    # success = 0 echec du test | 1 réussite du test
-    # previous = [cours qui vient d'etre quitté, précédent, 2e précédent, 3e précédent]
-    def retribution(previous,success):
-    	pheromones=10
-    	for i in range(0,3):
-    		var=pheromones/(i+2)
-    		link=Matrix.get(previous[i+1],previous[i])
-    		if success==1:
-    			link=Matrix.set(previous[i+1],previous[i],"plus",link[2]=link[2]+var)
-    		else
-				link=Matrix.set(previous[i+1],previous[i],"plus",link[2]=link[2]-var)
+	def test(self):
+		return "MON TEST"
