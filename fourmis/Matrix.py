834059e38f6206ee85c448091593c0c3f3b4cf82
Damien <teodam13@gmail.com>
2015-05-29 22:12:27 +0200

    Matrix

---
+++ fourmis/Matrix.py
@@ -0,0 +1,21 @@
+class Matrix(object):
+	# i = from
+	# j = to
+    def __init__():
+    	self.table=[] 
+    Matrix.add(1,2,1,3)
+    add(1,2,PHENOM_PLUS,  2)
+    add(1,2,PHENOM_MOINS, 2)
+    def set(i, j, k, ajout):
+    	if exists(i,j,k)
+    		#update
+    	else
+    		#add
+    	insert = [i,j,k]+[ajout];
+    	return self.table += insert
+
+    def get(i,j,k):
+    	return
+
+    def erosion(j,coeff):
+    	return
